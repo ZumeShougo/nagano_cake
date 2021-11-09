3f6870371ae9cbcf777b567722536957d4ac5721
EC2 Default User <ec2-user@ip-172-31-37-213.ec2.internal>
2021-11-09 12:32:29 +0000

    [update]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,22 @@
 class Public::OrdersController < ApplicationController
+  def index
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def create
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,53 @@
 Rails.application.routes.draw do
-  
+
   devise_for :customers, controllers: {
   sessions:      'customers/sessions',
   passwords:     'customers/passwords',
   registrations: 'customers/registrations'
   }
-  
+
   devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
   registrations: 'admins/registrations'
   }
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
       resources :genres, only:[:index, :create, :edit, :update]
       resources :customers, only:[:index, :show, :edit, :update]
       resources :items, only:[:index, :new, :create, :show, :edit, :update]
   end
-  
-  
+
+  namespace :public do
+
+    #homes
+    get '/' => 'homes#top'
+    get 'about' => 'homes#about'
+
+    #items
+    resources :items, only:[:index, :show]
+
+    #customers
+    get 'customers/my_page' => 'customers#show'
+    get 'customers/edit' => 'customers#edit'
+    patch 'customers' => 'customers#update'
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw'
+
+    #cart_items
+    resources :cart_items, only:[:index, :update, :create, :destroy]
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+
+    #orders
+    resources :orders, only:[:index, :show, :new, :create]
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/complete' => 'orders#complete'
+
+    #addresses
+    resources :addresses, only:[:index, :edit, :create, :update, :destroy]
+
+
+
+  end
 end
