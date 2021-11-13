05a685f7adfe5ae3478d9b0f2730fa2dc39bc1cd
EC2 Default User <ec2-user@ip-172-31-37-213.ec2.internal>
2021-11-13 07:06:01 +0000

    [add]top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,11 +1,11 @@
 class Public::HomesController < ApplicationController
-    
+
     def top
-        
+        @new_item = Item.last(4)
     end
-    
+
     def about
-        
+
     end
-    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,40 +14,54 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
-      resources :genres, only:[:index, :create, :edit, :update]
-      resources :customers, only:[:index, :show, :edit, :update]
-      resources :items, only:[:index, :new, :create, :show, :edit, :update]
-  end
-
-  namespace :public do
-
     #homes
     get '/' => 'homes#top'
-    get 'about' => 'homes#about'
 
-    #items
-    resources :items, only:[:index, :show]
+    #genres
+    resources :genres, only:[:index, :create, :edit, :update]
 
     #customers
-    get 'customers/my_page' => 'customers#show'
-    get 'customers/edit' => 'customers#edit'
-    patch 'customers' => 'customers#update'
-    get 'customers/unsubscribe' => 'customers#unsubscribe'
-    patch 'customers/withdraw' => 'customers#withdraw'
+    resources :customers, only:[:index, :show, :edit, :update]
 
-    #cart_items
-    resources :cart_items, only:[:index, :update, :create, :destroy]
-    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+    #items
+    resources :items, only:[:index, :new, :create, :show, :edit, :update]
 
     #orders
-    resources :orders, only:[:index, :show, :new, :create]
-    post 'orders/confirm' => 'orders#confirm'
-    get 'orders/complete' => 'orders#complete'
+    get 'admin/orders/:id' => 'orders#show'
+    patch '/admin/orders/:id' => 'orders#update'
 
-    #addresses
-    resources :addresses, only:[:index, :edit, :create, :update, :destroy]
-
-
+    #order_detailes
+    patch '/admin/orders/:order_id/order_details/:id' => 'order_detailes#update'
 
   end
+
+
+  #public_homes
+  get '/' => 'homes#top'
+  get 'about' => 'homes#about'
+
+  #public_items
+  resources :items, only:[:index, :show]
+
+  #public_customers
+  get 'customers/my_page' => 'customers#show'
+  get 'customers/edit' => 'customers#edit'
+  patch 'customers' => 'customers#update'
+  get 'customers/unsubscribe' => 'customers#unsubscribe'
+  patch 'customers/withdraw' => 'customers#withdraw'
+
+  #public_cart_items
+  resources :cart_items, only:[:index, :update, :create, :destroy]
+  delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+
+  #public_orders
+  resources :orders, only:[:index, :show, :new, :create]
+  post 'orders/confirm' => 'orders#confirm'
+  get 'orders/complete' => 'orders#complete'
+
+  #public_addresses
+  resources :addresses, only:[:index, :edit, :create, :update, :destroy]
+
+
+
 end
