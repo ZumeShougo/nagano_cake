a654636710cfab680b64843e0d851f54a3f7f70f
EC2 Default User <ec2-user@ip-172-31-37-213.ec2.internal>
2021-11-29 12:04:54 +0000

    [add]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,8 +10,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      #flash[:success] = "メールアドレスを更新しました"
-      redirect_to admin_items_path
+      redirect_to admin_item_path(item)
     else
       render :new
     end
